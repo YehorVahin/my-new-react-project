176165a35de8f6530db20432d00fd3bb8eb31b11
Yehor Vahin <davyjohns322@gmail.com>
2025-04-12 17:40:16 +0300

    change title

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,7 +2,7 @@ import Product from './Product';
 export default function App(){
   return(
     <div>
-      <h1>Best products</h1>
+      <h1>All the best products</h1>
 
       <Product 
       name="Tacos With Lime"

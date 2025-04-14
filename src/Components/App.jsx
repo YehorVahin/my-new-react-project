40221c487add61949a443c89c79320c3826874a7
Yehor Vahin <davyjohns322@gmail.com>
2025-04-14 15:59:03 +0300

    change Alert

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,4 +1,5 @@
 import Product from './Product';
+import { Alert } from './Alert';
 export default function App(){
   return(
     <div>
@@ -14,6 +15,18 @@ export default function App(){
       imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
       price={14.29} 
       />
+      <Alert variant="info">
+        Would you like to browse our recommended products?
+      </Alert>
+      <Alert variant="error">
+        There was an error during your last transaction
+      </Alert>
+      <Alert variant="success">
+        Payment received, thank you for your purchase
+      </Alert>
+      <Alert variant="warning">
+        Please update your profile contact information
+      </Alert>
     </div>
   );
 };
